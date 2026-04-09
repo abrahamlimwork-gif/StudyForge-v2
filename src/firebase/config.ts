a22ddb8ecccf958525e1c85e93418ed7d75a1be2
Trixia Lim <trixiaestraellim@gmail.com>
2026-04-09 09:05:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7696342024-5a094",
+  "appId": "1:210492515699:web:6bb667772a887980999106",
+  "apiKey": "AIzaSyDzP1psgugjSpfr5HptIVV0qzxzvznBtcI",
+  "authDomain": "studio-7696342024-5a094.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "210492515699"
+};
