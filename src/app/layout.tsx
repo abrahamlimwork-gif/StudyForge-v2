54a754f69264e685367d2986accadea42bfde3d4
Trixia Lim <trixiaestraellim@gmail.com>
2026-04-09 17:01:39 +0000

    Fix the hydration mismatch error in my src/app/layout.tsx file. The erro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -22,7 +22,7 @@ export default function RootLayout({
         {/* Using vpaas.jitsi.net script as requested */}
         <script src="https://vpaas.jitsi.net/external_api.js" async></script>
       </head>
-      <body className="font-body antialiased min-h-screen">
+      <body className="font-body antialiased min-h-screen" suppressHydrationWarning>
         <FirebaseClientProvider>
           {children}
           <Toaster />
